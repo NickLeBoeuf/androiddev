542656a54c0bc5726dea601408d201e8a5e7f053
Nick <nick.leboeuf@gmail.com>
2021-02-25 01:15:50 +0100

    Enter new Itinerary fragment is having several features: - date selector - KM buttons are functionning
    -> Next:
    - Optimize Text "destination" usage
    -  Open a popup summarizing the entry and ask for confirmation
    - Create all the backend stuff to store entries in a file, and read it back to display history

--- app/src/main/java/com/example/kmlogger/SecondFragment.kt
+++ app/src/main/java/com/example/kmlogger/SecondFragment.kt
@@ -1,6 +1,8 @@
 package com.example.kmlogger
 
+import android.content.Context
 import android.os.Bundle
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.chip.Chip
 import java.util.*
 
 /**
@@ -15,12 +18,16 @@ import java.util.*
  */
 class SecondFragment : Fragment() {
 
+    lateinit var datePicker: DatePickerHelper
+    var kmentry : Int = 0
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
 
-
+        val activity = activity as Context
+        datePicker = DatePickerHelper(activity)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
@@ -30,13 +37,43 @@ class SecondFragment : Fragment() {
 
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
+        val month = c.get(Calendar.MONTH) + 1
         val day = c.get(Calendar.DAY_OF_MONTH)
 
         view.findViewById<TextView>(R.id.textDate).text = "$day / $month / $year"
+        view.findViewById<TextView>(R.id.textDate).setOnClickListener{showDatePickerDialog(view)}
 
         view.findViewById<Button>(R.id.button_secondback).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        view.findViewById<Chip>(R.id.chip_plus1).setOnClickListener{ addKm(1,view) }
+        view.findViewById<Chip>(R.id.chip_minus1).setOnClickListener{ addKm(-1,view) }
+        view.findViewById<Chip>(R.id.chip_plus10).setOnClickListener{ addKm(10,view) }
+        view.findViewById<Chip>(R.id.chip_minus10).setOnClickListener{ addKm(-10,view) }
+        view.findViewById<Chip>(R.id.chip_plus100).setOnClickListener{ addKm(100,view) }
+        view.findViewById<Chip>(R.id.chip_minus100).setOnClickListener{ addKm(-100,view) }
+
+
+    }
+
+    fun addKm(value: Int,view : View) {
+        kmentry = kmentry + value
+        view.findViewById<TextView>(R.id.kmNumber).text = kmentry.toString() + " Km"
+    }
+
+    private fun showDatePickerDialog(view: View) {
+        val cal = Calendar.getInstance()
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+        val m = cal.get(Calendar.MONTH)
+        val y = cal.get(Calendar.YEAR)
+        datePicker.showDialog(d, m, y, object : DatePickerHelper.Callback {
+            override fun onDateSelected(dayofMonth: Int, month: Int, year: Int) {
+                val dayStr = if (dayofMonth < 10) "0${dayofMonth}" else "${dayofMonth}"
+                val mon = month + 1
+                val monthStr = if (mon < 10) "0${mon}" else "${mon}"
+                view.findViewById<TextView>(R.id.textDate).text = "${dayStr}/${monthStr}/${year}"
+            }
+        })
     }
 }
